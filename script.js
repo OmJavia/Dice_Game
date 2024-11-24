8799acfae724a2c63f479b2d005d2ad9e508b5d6
Om Javia <omjavia18@gmail.com>
2024-11-24 14:23:49 +0530

    Added Sound & Icon

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+// Adding the dice sound
+const diceSound = new Audio('./Sound/dice.mp3'); 
 
 let scores, currentScore, activePlayer, playing;
 
@@ -46,12 +48,16 @@ const switchPlayer = function () {
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
+    // Play the dice sound
+    diceSound.currentTime = 0; // Reset sound to the start
+    diceSound.play();
+
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `./Images/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
@@ -71,7 +77,6 @@ btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
-    // scores[1] = scores[1] + currentScore
 
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
